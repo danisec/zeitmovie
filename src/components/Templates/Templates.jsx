d7c570235d43c4e06207fb197638eff27b4a6a38
danisec <daniaprilyanto35@gmail.com>
2022-11-30 18:52:50 +0700

    add route HomePage

--- src/components/Templates/Templates.jsx
+++ src/components/Templates/Templates.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import Navbar from '../UI/organisms/navbar/Navbar'
+
+import HomePage from '../pages/home'
+
+import Footer from '../UI/organisms/footer/Footer'
+
 function Templates() {
-  return <></>
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+
+      <Footer />
+    </>
+  )
 }
 
 export default Templates
